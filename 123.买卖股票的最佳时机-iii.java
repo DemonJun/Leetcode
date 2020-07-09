1a6bb83120ca56df5f0f1e8811093036219e5353
DemonJun <demonjun@foxmail.com>
2020-07-09 20:46:55 +0800

    add

--- 123.买卖股票的最佳时机-iii.java
+++ 123.买卖股票的最佳时机-iii.java
@@ -12,18 +12,19 @@ class Solution {
         if (prices.length < 2) {
             return 0;
         }
-        // int[i][j][k] -> i表示第i+1天；j表示第j+1次买股票；k-> 0表示持有、1表示卖出
-        int k = 2;
-        int[][][] dp = new int[prices.length][k][2];
-        dp[0][0][1] = -prices[0];
+        // int[i][j][k] -> i表示第i天；j表示第j次买股票；k-> 0表示不持有股票、1表示持有股票
+        int maxTime = 2;
+        int[][][] dp = new int[prices.length + 1][maxTime + 1][2];
+        dp[0][1][1] = -prices[0];
+        dp[0][2][1] = -prices[0];
         for (int i = 1; i < prices.length; i++) {
-            for (int j = 1; j < k; j++) {
-                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] - prices[i]);
-                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] + prices[i]);
+            for (int j = 1; j <= 2; j++) {
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
             }
         }
 
-        return dp[prices.length - 1][k - 1][0];
+        return dp[prices.length - 1][maxTime][0];
     }
 }
 // @lc code=end
